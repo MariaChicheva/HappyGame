d8e04b69358fdc71fa25c3ed7af7eb4efdfabfd6
MariRia <masha.chicheva2005@gmail.com>
2026-05-17 00:44:55 +0400

    Added quick exit from the game via the Escape key.

--- HappyGame/MainWindow.xaml.cs
+++ HappyGame/MainWindow.xaml.cs
@@ -15,14 +15,19 @@ using System.Windows.Shapes;
 
 namespace HappyGame
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
         }
     }
 }
