4f1159a3f6ad8cc6799ea11987ddd59708f49780
MariRia <masha.chicheva2005@gmail.com>
2026-05-19 02:22:26 +0400

    Add road animation.

--- HappyGame/MainWindow.xaml.cs
+++ HappyGame/MainWindow.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace HappyGame
 {
@@ -20,6 +22,7 @@ namespace HappyGame
         public MainWindow()
         {
             InitializeComponent();
+            StartRoadAnimation();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -28,6 +31,27 @@ namespace HappyGame
             {
                 this.Close();
             }
+        }
+
+        private void StartRoadAnimation()
+        {
+            double roadHeight = 648.5;
+
+            DoubleAnimation anim = new DoubleAnimation();
+            anim.From = 0;
+            anim.To = roadHeight;
+            anim.Duration = TimeSpan.FromSeconds(5);
+            anim.RepeatBehavior = RepeatBehavior.Forever;
+
+            Road.BeginAnimation(Canvas.TopProperty, anim);
+
+            DoubleAnimation anim2 = new DoubleAnimation();
+            anim2.From = -roadHeight;
+            anim2.To = 0;
+            anim2.Duration = TimeSpan.FromSeconds(5);
+            anim2.RepeatBehavior = RepeatBehavior.Forever;
+
+            RoadBack.BeginAnimation(Canvas.TopProperty, anim2);
         }
     }
 }
